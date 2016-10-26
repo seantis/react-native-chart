db9fd151f24b2080787143960395cd152c77b27a
Marc Sommerhalder <marc.sommerhalder@seantis.ch>
2016-10-26 09:41:47 +0200

    Fix horizontal grid

--- src/Grid.js
+++ src/Grid.js
@@ -27,11 +27,11 @@ export default class Grid extends Component {
 		const data = this.props.data || [[]];
 		const unique = uniqueValuesInDataSet(data[0]);
 		const horizontalSteps = (unique.length < this.props.verticalGridStep) ? unique.length : this.props.verticalGridStep;
-		let stepsBetweenVerticalLines = this.props.horizontalGridStep ? Math.round(data.length / this.props.horizontalGridStep) : 1;
+		let stepsBetweenVerticalLines = this.props.horizontalGridStep ? Math.round(data[0].length / this.props.horizontalGridStep) : 1;
 		if (stepsBetweenVerticalLines < 1) stepsBetweenVerticalLines = 1;
 
 		for (let i = horizontalSteps; i > 0; i--) horizontalRange.push(i);
-		for (let i = data.length - 1; i > 0; i -= stepsBetweenVerticalLines) verticalRange.push(i);
+		for (let i = data[0].length - 1; i > 0; i -= stepsBetweenVerticalLines) verticalRange.push(i);
 
 		const containerStyle = { width: this.props.width, height: this.props.height, position: 'absolute', left: 0 };
 
@@ -49,7 +49,7 @@ export default class Grid extends Component {
 
 		const verticalGridStyle = {
 			height: this.props.height + 1,
-			width: (this.props.width / (data.length - 1)) * stepsBetweenVerticalLines,
+			width: (this.props.width / (data[0].length - 1)) * stepsBetweenVerticalLines,
 			borderRightColor: this.props.gridColor,
 			borderRightWidth: intendedLineWidth,
 		};
